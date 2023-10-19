57baadc7259edb0b96b81069c49fd0e42eb8fce6
Myroslav Ferendovych <w67927@student.wsiz.edu.pl>
2023-10-19 15:56:45 +0200

    lab2

---
+++ ConsoleApp1/Constructions/Constructions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Construction
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entrances,
+            int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = Width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial
+
+        }
+            
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+
+    }
+}
+
+
+
+
+1.Height typu zmiennoprzecinkowego
+2. Width typu zmiennoprzecinkowego
+3. Entrances typu liczba całkowita
+4. HumanCapacity typu liczba całkowita
+5. BuildMaterial typu tekstowego
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿ using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(23, 45, 4, 6, "Brik");
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
